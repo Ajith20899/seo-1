7f4754758ac43b4a27e7b204d2b36599325d75bb
Ajith-leet <ajithkumar@paperflite.com>
2025-01-21 00:14:55 +0530

    inital commit 7

--- src/pages/[[...alias]].js
+++ src/pages/[[...alias]].js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import React from "react";
 
 function page() {
-  const [alias, setAlias] = React.useState("ascas");
+  const [alias, setAlias] = React.useState("ascasgv");
 
   return <React.Fragment>{alias && <Metadata />}</React.Fragment>;
 }
