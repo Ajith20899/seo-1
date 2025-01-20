3761634c5946e4ec00c76e9c94166a065007237e
Ajith-leet <ajithkumar@paperflite.com>
2025-01-21 00:10:47 +0530

    inital commit

--- src/pages/[[...alias]].js
+++ src/pages/[[...alias]].js
@@ -2,26 +2,31 @@ import Head from "next/head";
 import React from "react";
 
 function page() {
+  const [alias, setAlias] = React.useState("ascas");
 
-    console.log("Entered");
-    
+  console.log("Entered");
 
-  return (
-    <React.Fragment>
-      <Head>
-        <title>SEO Next App</title>
-        <meta name="description" content="SEO by create next app" />
-        <meta
-          property="og:image"
-          content={
-            "https://images.pexels.com/photos/7303342/pexels-photo-7303342.jpeg?auto=compress&cs=tinysrgb&w=800&h=600&lazy=load"
-          }
-        />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-      </Head>
-    </React.Fragment>
-  );
+  return <React.Fragment>{alias && <Metadata />}</React.Fragment>;
 }
+
+const Metadata = () => {
+  return (
+    <Head>
+      <title data-next-head="">SEO Next App 1</title>
+      <meta
+        name="description"
+        content="SEO by create next app"
+        data-next-head=""
+      />
+      <meta
+        property="og:image"
+        content="https://images.pexels.com/photos/7303342/pexels-photo-7303342.jpeg?auto=compress&amp;cs=tinysrgb&amp;w=800&amp;h=600&amp;lazy=load"
+        data-next-head=""
+      />
+      <meta property="og:image:width" content="1200" data-next-head="" />
+      <meta property="og:image:height" content="630" data-next-head="" />
+    </Head>
+  );
+};
 
 export default page;
