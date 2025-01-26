9b4bfaddf7fcd2510b4791cba345eea0918b7bcc
ajithsachinrohit@gmail.com <ajithsachinrohit@gmail.com>
2025-01-26 21:18:01 +0530

    commit

--- src/SEO.js
+++ src/SEO.js
@@ -10,24 +10,15 @@ const Metadata = () => {
 
   return (
     <Head>
-      <title data-next-head="">{user?.name}</title>
-      <meta
-        name="description"
-        content={user?.email}
-        data-next-head=""
-      />
-            <meta
-        property="og:title"
-        content={user?.website}
-        data-next-head=""
-      />
+      <title>{user?.name}</title>
+      <meta name="description" content={user?.email} />
+      <meta property="og:title" content={user?.website} />
       <meta
         property="og:image"
         content="https://images.pexels.com/photos/7303342/pexels-photo-7303342.jpeg?auto=compress&amp;cs=tinysrgb&amp;w=800&amp;h=600&amp;lazy=load"
-        data-next-head=""
       />
-      <meta property="og:image:width" content="1200" data-next-head="" />
-      <meta property="og:image:height" content="630" data-next-head="" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
     </Head>
   );
 };
