ac2f6bbed2844a21a937db62b377f4ca7be6ece9
ajithsachinrohit@gmail.com <ajithsachinrohit@gmail.com>
2025-01-26 21:11:27 +0530

    commit

--- src/pages/[[...alias]].js
+++ src/pages/[[...alias]].js
@@ -8,25 +8,42 @@ function page() {
     console.log("useEffect");
   }, []);
 
-  return <Metadata />
+  return (
+    <React.Fragment>
+      <Metadata />
+      {/* other page content */}
+      <OtherPage />
+    </React.Fragment>
+  );
 }
 
-export const getServerSideProps = wrapper.getServerSideProps(store => async () => {
-  try {
-    store.dispatch({
-      type: 'FETCH_USERS_REQUEST',
-      payload: 'Server side payload'
-    });
-  
-    return {
-      props: { users: [] }, // will be passed to the page component as props
-    };
-  } catch (error) {
-    console.error('Failed to fetch users:', error);
-    return {
-      props: { users: [] },
-    };
+function OtherPage() {
+  useEffect(() => {
+    console.log("OtherPage");
+  }, []);
+
+  // Other page content
+  return <div>Home</div>;
+}
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async () => {
+    try {
+      store.dispatch({
+        type: "FETCH_USERS_REQUEST",
+        payload: "Server side payload",
+      });
+
+      return {
+        props: { users: [] }, // will be passed to the page component as props
+      };
+    } catch (error) {
+      console.error("Failed to fetch users:", error);
+      return {
+        props: { users: [] },
+      };
+    }
   }
-});
+);
 
 export default page;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,18 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="new name" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          href="/fonts/icomoon/cleverstory/style.css"
+          rel="stylesheet"
+        ></link>
+        <link href="/fonts/aktiv-grotesk/style.css" rel="stylesheet"></link>
+
+        <link rel="stylesheet" href="/css/slick/slick.css" />
+        <link rel="stylesheet" href="/css/slick/slicktheme.css" />
       </Head>
       <body>
         <Main />
