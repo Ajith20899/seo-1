69008933842537018c9eb30567897b1f58fdca44
ajithsachinrohit@gmail.com <ajithsachinrohit@gmail.com>
2025-01-26 21:25:32 +0530

    commit

--- src/SEO.js
+++ src/SEO.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import React from "react";
 import { useSelector } from "react-redux";
 
 const Metadata = () => {
@@ -9,17 +10,20 @@ const Metadata = () => {
   const user = users?.[0];
 
   return (
-    <Head>
-      <title>{user?.name}</title>
-      <meta name="description" content={user?.email} />
-      <meta property="og:title" content={user?.website} />
-      <meta
-        property="og:image"
-        content="https://images.pexels.com/photos/7303342/pexels-photo-7303342.jpeg?auto=compress&amp;cs=tinysrgb&amp;w=800&amp;h=600&amp;lazy=load"
-      />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
-    </Head>
+    <React.Fragment>
+      {" "}
+      <Head>
+        <title>{user?.name}</title>
+        <meta name="description" content={user?.email} />
+        <meta property="og:title" content={user?.website} />
+        <meta
+          property="og:image"
+          content="https://images.pexels.com/photos/7303342/pexels-photo-7303342.jpeg?auto=compress&amp;cs=tinysrgb&amp;w=800&amp;h=600&amp;lazy=load"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
+    </React.Fragment>
   );
 };
 
